b8578ce9c714f5c8298e37bef1fdb36b8d03d5cb
eddys999 <eddy20003@gmail.com>
2023-02-24 13:08:40 +0100

    replace list with a set to avoid duplicates

--- tweet.py
+++ tweet.py
@@ -100,23 +100,23 @@ def display(G):
     nx.draw(G, node_color=node_color, edge_color=edge_color, node_size=node_size, with_labels=True)
     plt.show()
 
-
-def browsing_graph(G, size, numberOfsentences):
-    for i in range(numberOfsentences):
-        actual_node = random.choice(list(G.nodes()))
-        visited_node = []
-        visited_node.append(actual_node)
-        for j in range(1,size):
-            node_neighbors = list(G.neighbors(actual_node))
-            if node_neighbors:
+#its very slow to generate more than one sentence so the limit its one 
+def browsing_graph(G, size, numberOfsentences=1):
+    with open("generated_sentence.txt","w", encoding="UTF-8") as f:
+        for i in range(numberOfsentences):
+            actual_node = random.choice(list(G.nodes()))
+            visited_node = set([actual_node])
+            for j in range(1,size):
+                node_neighbors = list(G.neighbors(actual_node))
+                if not node_neighbors:
+                    break
                 actual_node = random.choice(node_neighbors)
-                visited_node.append(actual_node)
-            else:
-                break
-        output = " ".join([str(k) for k in visited_node])
-        print(output)
-        outputInTxt = output.replace(' ',';')
-        with open("generated_sentence.txt","w", encoding="UTF-8") as f:
+                visited_node.add(actual_node)
+                
+            output = " ".join([str(k) for k in visited_node])
+            print(output)
+            outputInTxt = output.replace(' ',';')
+       
             f.write(outputInTxt)
 
         
